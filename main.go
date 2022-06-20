a4db46e32f92117e321a891486dbc06024799f67
falconhan <103387957+falconhan@users.noreply.github.com>
2022-06-20 22:53:17 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,16 +1,1 @@
-package main
 
-import "fmt"
-
-func main() {
-	arr := []int{4, 1, 4, -4, 6, 3, 8, 8}
-	var result []int
-
-	for i := 1; i < len(arr); i++ {
-		if arr[i] != arr[i-1] {
-			result = append(result, arr[i])
-		}
-	}
-	fmt.Println(result)
-
-}
